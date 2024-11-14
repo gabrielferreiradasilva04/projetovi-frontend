7e126b44f5eac75069e70caf0be2c6ae0d9e80fd
Gabriel Silva <gabrielferreirasilva04@outlook.com>
2024-11-14 00:55:37 -0300

    adição e atualização de estoques

--- src/stores/stockStore.js
+++ src/stores/stockStore.js
@@ -1,6 +1,5 @@
 import { defineStore } from "pinia";
 import { api } from "@/services/axiosConfig";
-import { withCtx } from "vue";
 
 export const useStockStore = defineStore('stock', {
     state: () => ({
@@ -16,6 +15,13 @@ export const useStockStore = defineStore('stock', {
     actions: {
         addStockOnUserStocks(stock) {
             this.userStocks.push(stock);
+        },
+        updateUserStocksAfterUpdateList(stock) {
+            let oldStockIndex = this.userStocks.findIndex(item => item.id === stock.id);
+
+            if (oldStockIndex != -1) {
+                this.userStocks.splice(oldStockIndex, 1, stock);
+            }
         },
         async getAuthenticatedUserStocks() {
             this.snackbarConfig.color = '';
@@ -75,12 +81,13 @@ export const useStockStore = defineStore('stock', {
                             color: 'success'
                         }
                         this.addStockOnUserStocks(request.data);
-                    }
-                } else {
-                    this.snackbarConfig = {
-                        message: 'Estoque Inválido',
-                        time: 2000,
-                        color: 'error'
+                    } else {
+                        this.snackbarConfig = {
+                            message: 'Estoque Inválido',
+                            time: 2000,
+                            color: 'error'
+                        }
+                        console.log(request.data)
                     }
                 }
             } catch (error) {
@@ -97,6 +104,84 @@ export const useStockStore = defineStore('stock', {
                             time: 2000,
                             color: 'error'
                         }
+                        console.log(error)
+                    }
+                } else {
+                    this.snackbarConfig = {
+                        message: 'Tente novamente mais tarde',
+                        time: 2000,
+                        color: 'warning'
+                    }
+                    console.log(error)
+                }
+            }
+        },
+        async updateStockFields(stock) {
+            this.snackbarConfig.color = '';
+            this.snackbarConfig.time = 0;
+            this.snackbarConfig.message = '';
+            try {
+                if (stock == null) {
+                    this.snackbarConfig = {
+                        message: 'Estoque inexistente',
+                        time: 2000,
+                        color: 'warning'
+                    }
+                    return;
+                }
+                else if (stock.id === '') {
+                    this.snackbarConfig = {
+                        message: 'Estoque inexistente',
+                        time: 2000,
+                        color: 'warning'
+                    }
+                    return;
+                }
+                else {
+                    const stockData = {
+                        id: stock.id,
+                        title: stock.title,
+                        description: stock.description,
+                        address: stock.address,
+                        contact: stock.contact,
+                        totalCapacity: stock.totalCapacity
+                    }
+
+                    const request = await api.put(
+                        '/stocks/' + stockData.id,
+                        stockData, { withCredentials: true }
+                    );
+                    if (request.status === 200) {
+                        this.snackbarConfig = {
+                            message: 'Atualizado com sucesso',
+                            time: 2000,
+                            color: 'success'
+                        }
+                        this.updateUserStocksAfterUpdateList(request.data);
+                    } else {
+                        this.snackbarConfig = {
+                            message: 'Estoque Inválido',
+                            time: 2000,
+                            color: 'error'
+                        }
+                        console.log(request.data)
+                    }
+                }
+            } catch (error) {
+                if (error.response) {
+                    if (error.response.status === 403) {
+                        this.snackbarConfig = {
+                            message: 'Não autorizado',
+                            time: 2000,
+                            color: 'error'
+                        }
+                    } else {
+                        this.snackbarConfig = {
+                            message: error.response.data.message,
+                            time: 2000,
+                            color: 'error'
+                        }
+                        console.log(error)
                     }
                 } else {
                     this.snackbarConfig = {
